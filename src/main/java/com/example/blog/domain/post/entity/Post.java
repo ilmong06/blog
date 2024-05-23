5ba9b993d82725c61da356a6852ef51d8d062dbc
ilmong06 <ilmongcoding@gmail.com>
2024-05-23 17:28:30 +0900

    1

---
+++ src/main/java/com/example/blog/domain/post/entity/Post.java
@@ -0,0 +1,23 @@
+package com.example.blog.domain.post.entity;
+
+import com.example.blog.domain.member.entity.Member;
+import com.example.blog.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Post extends BaseEntity {
+    private String title;
+    private String content;
+    @ManyToOne
+    private Member author;
+}
